8152e841e50820d9a62a6297f9be2e24fed5edca
shaonianruntu <514574496@qq.com>
2019-10-08 16:50:48 +0800

    CSS Transition 动画的实现

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Description:
+ * @Author: fangn
+ * @Github:
+ * @Date: 2019-10-08 16:15:44
+ * @LastEditors: fangn
+ * @LastEditTime: 2019-10-08 16:45:55
+ */
+import React, { Component, Fragment } from "react";
+import { CSSTransition } from "react-transition-group";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// import "./style.css";
+import "./CSSTransition_style.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <CSSTransition
+          in={this.state.show}
+          timeout={1000}
+          classNames="fade"
+          unmountOnExit
+          onEntered={el => {
+            el.style.color = "yellow";
+          }}
+          appear={true}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div>hello</div>
+        </CSSTransition>
+        {/* <div className={this.state.show ? "show" : "hide"}>hello</div> */}
+        <button onClick={this.handleToggle.bind(this)}>toggle</button>
+      </Fragment>
+    );
+  }
+
+  handleToggle() {
+    this.setState(() => ({
+      show: this.state.show ? false : true
+    }));
+  }
 }
 
 export default App;
