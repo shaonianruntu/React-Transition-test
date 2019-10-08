654b6d92b1ce4cd74b370706dc33e128e3d0583e
shaonianruntu <514574496@qq.com>
2019-10-08 16:57:59 +0800

    Transition Group 动画的实现

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@
  * @Github:
  * @Date: 2019-10-08 16:15:44
  * @LastEditors: fangn
- * @LastEditTime: 2019-10-08 16:45:55
+ * @LastEditTime: 2019-10-08 16:57:10
  */
 import React, { Component, Fragment } from "react";
-import { CSSTransition } from "react-transition-group";
+// import { CSSTransition } from "react-transition-group";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 // import "./style.css";
 import "./CSSTransition_style.css";
@@ -16,14 +17,35 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: true
+      // show: true
+      list: []
     };
+    this.handleAddItem = this.handleAddItem.bind(this);
   }
 
   render() {
     return (
       <Fragment>
-        <CSSTransition
+        {/* <div>hello</div> */}
+        <TransitionGroup>
+          {this.state.list.map((item, index) => {
+            return (
+              <CSSTransition
+                timeout={1000}
+                classNames="fade"
+                unmountOnExit
+                onEntered={el => {
+                  el.style.color = "yellow";
+                }}
+                appear={true}
+              >
+                <div key={index}>{item}</div>
+              </CSSTransition>
+            );
+          })}
+        </TransitionGroup>
+        <button onClick={this.handleAddItem}>Submit</button>
+        {/* <CSSTransition
           in={this.state.show}
           timeout={1000}
           classNames="fade"
@@ -34,11 +56,17 @@ class App extends Component {
           appear={true}
         >
           <div>hello</div>
-        </CSSTransition>
+        </CSSTransition> */}
         {/* <div className={this.state.show ? "show" : "hide"}>hello</div> */}
-        <button onClick={this.handleToggle.bind(this)}>toggle</button>
+        {/* <button onClick={this.handleToggle.bind(this)}>toggle</button> */}
       </Fragment>
     );
+  }
+
+  handleAddItem() {
+    this.setState(prevState => ({
+      list: [...prevState.list, "item"]
+    }));
   }
 
   handleToggle() {
